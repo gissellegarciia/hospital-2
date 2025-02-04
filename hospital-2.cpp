af9b9a01ca7cd31f94c2707aa0cad017005ee228
gissellegarciia <gisellegl@msmk.university>
2025-02-04 18:30:44 +0100

    Agregar archivos de proyecto.

---
+++ hospital-2.cpp
@@ -0,0 +1,12 @@
+﻿// hospital-2.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "hospital-2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
